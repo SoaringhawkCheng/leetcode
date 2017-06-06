9f76196d2bfe8a687c19b3cea0ef0a5e05616935
Soaringhawk Cheng <2856043766@qq.com>
2017-06-06 19:20:31 +0800

    20170606

---
+++ leetcode/146.cpp
@@ -0,0 +1,10 @@
+//
+//  146.cpp
+//  leetcode
+//
+//  Created by 追寻梦之碎片 on 2017/5/27.
+//  Copyright © 2017年 追寻梦之碎片. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
---
+++ leetcode/189.cpp
@@ -0,0 +1,9 @@
+//
+//  189.cpp
+//  leetcode
+//
+//  Created by 追寻梦之碎片 on 2017/6/6.
+//  Copyright © 2017年 追寻梦之碎片. All rights reserved.
+//
+
+#include <stdio.h>
